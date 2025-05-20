4f56eaf2976f7f2cc5b3a710de712e2c203c0148
oly619 <bozzmcpolly@gmail.com>
2025-05-20 15:16:55 +0300

    initial commit

---
+++ polls/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+
+
+class Question(models.Model):
+    def __str__(self):
+        return self.question_text
+    
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+class choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+    
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def was_published_recently(self):
+     now = timezone.now()
+     return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+# Create your models h
